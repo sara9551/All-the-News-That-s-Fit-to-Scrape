a36975e1ab3d7743f258b09fc1db62b5145bfd01
Sara Anspach <sara.anspach@gmail.com>
2018-10-21 20:05:18 -0400

    test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database//
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
